84afc335ff8b18518809f9416212482487199f1d
黄章成 <huangzhangcheng@liminwang.local>
2017-07-31 13:55:55 +0800

    Initial Commit

---
+++ IndicatorViewTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  IndicatorViewTest
+//
+//  Created by 黄章成 on 2017/7/31.
+//  Copyright © 2017年 黄章成. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ IndicatorViewTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  IndicatorViewTest
+//
+//  Created by 黄章成 on 2017/7/31.
+//  Copyright © 2017年 黄章成. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
